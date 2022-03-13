ff90fcda88c67677e26f2e998b79071b0a2de150
stepa <stepa@NB-STEPAN>
2022-03-13 21:05:06 +0100

    Přidejte soubory projektu.

---
+++ Rekurze.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+float urok(int x) {
+	if (x <= 1) return x * 1.01;
+	else return urok(x - 1) * 1.01;
+}
+
+int main() {
+	printf("Úrok:\n");
+	printf("%f\n", urok(1));
+	printf("%f\n", urok(5));
+	printf("%f\n", urok(10));
+	printf("%f\n", urok(15));
+}
